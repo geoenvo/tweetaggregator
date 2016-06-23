fced4ce7f128c4a48d198767f9c4f328e02b874b
Seno @ ThinkPad <senoadiw@gmail.com>
2016-06-23 13:00:41 +0700

    fix: default db provisioning, update: static file settings, settings example

--- tweetaggregator/tweetaggregator/urls.py
+++ tweetaggregator/tweetaggregator/urls.py
@@ -19,3 +19,13 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
