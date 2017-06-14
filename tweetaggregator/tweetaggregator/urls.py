cd0f253352f503ced4644e24decc85be606c0d93
Seno A. K <senoadiw@gmail.com>
2017-06-14 17:30:25 +0700

    new: production settings

--- tweetaggregator/tweetaggregator/urls.py
+++ tweetaggregator/tweetaggregator/urls.py
@@ -21,21 +21,36 @@ from django.views.generic import TemplateView
 import taggit_autosuggest.urls
 
 
-urlpatterns = [
+admin_url_pattern = [url(r'^admin/', admin.site.urls)]
+
+if getattr(settings, 'ADMIN_URL_PATTERN', False):
+    admin_url_pattern = [url(getattr(settings, 'ADMIN_URL_PATTERN'), admin.site.urls)]
+
+urlpatterns = admin_url_pattern + [
     url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'^charts/', include('charts.urls')),
     url(r'^twitter/', include('twitter.urls')),
     url(r'^taggit_suggest/', include('taggit_autosuggest.urls')),
 ]
 
 
+# Allow dev server to serve media files
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+elif getattr(settings, 'FORCE_SERVE_STATIC', False): # If set to True force dev server to serve static files
+    settings.DEBUG = True
     urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT
-    )
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    settings.DEBUG = False
+
+# Change admin header text
+if getattr(settings, 'ADMIN_SITE_HEADER', False):
+    admin.site.site_header = getattr(settings, 'ADMIN_SITE_HEADER')
+if getattr(settings, 'ADMIN_INDEX_TITLE', False):
+    admin.site.index_title = getattr(settings, 'ADMIN_INDEX_TITLE')
+if getattr(settings, 'ADMIN_SITE_TITLE', False):
+    admin.site.site_title = getattr(settings, 'ADMIN_SITE_TITLE')
